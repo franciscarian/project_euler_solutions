35809085ef4b480717f8389ab7a3c568a3dbced1
franciscarian <fjames.carian@gmail.com>
2026-04-09 17:40:44 +0100

    Solution found!
    Changed it so that limit cannot be one of the numbers summed. (misread problem)
    
    Changed to remove multiples of 15, since they were already accounted for by the fives and threes

--- problem_1.py
+++ problem_1.py
@@ -4,12 +4,13 @@
 def multiples_sum(multiple, limit):
     sum = 0
     i = 0
-    while i*multiple <= limit:
+    while i*multiple < limit:
         sum += i*multiple
         i += 1
     return sum
 
 fives = multiples_sum(5, 1000)
 threes = multiples_sum(3, 1000)
+fifteens = multiples_sum(15, 1000)
 
-print(fives+threes)
+print((fives+threes) - fifteens)
