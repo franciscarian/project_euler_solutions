91514a20286f897ed4b9683798400ec11fa1cbb3
franciscarian <fjames.carian@gmail.com>
2026-04-09 18:05:26 +0100

    imported function to utilities class

--- utilities.py
+++ utilities.py
@@ -1,6 +1,14 @@
 class Euler_Utilies:
     def __init__(self):
-        self.color = ''
+        pass
     
     def print_message(self, message):
         print(f"This is a test! Message: {message}")
+        
+    def multiples_sum(self, multiple, limit):
+        sum = 0
+        i = 0
+        while i*multiple < limit:
+            sum += i*multiple
+            i += 1
+        return sum
